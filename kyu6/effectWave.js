0b22f2f0150b4f98eb03e15788065399fb4a8ba1
Elmar Erzikhanov <elmarerzikhanov@MacBook-Pro-Elmar.local>
2023-09-13 10:58:33 +0300

    commit master 8

--- kyu6/effectWave.js
+++ kyu6/effectWave.js
@@ -31,32 +31,89 @@
 // console.log(effectWave('elmar', 5)); // Elmar eLmar elMar elmAr elmaR 
 
 // Решение 2
-
 // const effectWave = (str, num) => {
-//   const array = [];
-//   for(let i = 0; i < num; i++){
-//     array[i] = str; // [ 'elmar', 'elmar', 'elmar', 'elmar', 'elmar' ]
-//   }
-//   let string = '';
-//   let k =  0;
-//   for(let i = 0; i < array.length; i++){
-//    for(let j = 0; j < array[i].length; j++){
-//     if(j === k){
-//       string += array[i][k].toUpperCase();
-//     }else {
-//       string += array[i][j];
+//     const array = [];
+//     for(let i = 0; i < num; i++){
+//         array.push(str);
 //     }
+//     let string = '';
+//     for(let i = 0; i < array.length; i++){
+//         for(let j = 0; j < array[i].length; j++){
+//             if(j === i){
+//                 string += array[i][j].toUpperCase();
+//             }else {
+//                 string += array[i][j];
+//             }
+//         }
+//         string += ' '
+//     }
+//     return string
+// }
+// console.log(effectWave('elmar', 5)); // Elmar eLmar elMar elmAr elmaR 
+
+
+// Решение 3
+// const effectWave = (str, num) => {
+//    let string = '';
+//    let k = 0;
+//    const array = Array(num).fill(str);
+//    for(let i = 0; i < array.length; i++){
+//     string += array[i].slice(0,i) + array[i][k].toUpperCase() + array[i].slice(i+1) + ' ';
+//     k++;
 //    }
-//    k++;
-//   }
-//   let finStr = '';
-//   let z = num;
-//   while(z <= string.length){
-//     finStr += string.slice(z-num,z) + ' '
-//     z+=num;
-//   }
-//   return finStr
+//    return string
+// }
+// console.log(effectWave('elmar', 5)); // Elmar eLmar elMar elmAr elmaR 
+
+
+// Решение 4
+// const helpFunction = (item, index) => {
+//     return item.slice(0,index) + item[index].toUpperCase() + item.slice(index + 1);
+// }
+// const effectWave = (str, num) => {
+//     let string = '';
+//     const array = Array(num).fill(str);
+//     array.forEach((elem, index) => {
+//         string += helpFunction(elem, index) + ' ';
+//     });
+//     return string
 // }
 // console.log(effectWave('elmar', 5)); // Elmar eLmar elMar elmAr elmaR
 
 
+// Решение 5
+// const helpFunction = (item, index) => {
+//     let str = '';
+//     for(let i = 0; i < item.length; i++){
+//         if(i === index){
+//             str += item[index].toUpperCase();
+//         }else {
+//             str += item[i];
+//         }
+       
+//     }
+//     return str + ' ';
+// }
+// const effectWave = (str, num) => {
+//     const array = [];
+//     for(let i = 0; i < num; i++) {array.push(str)};
+//     let string =  '';
+//     for(let i = 0; i < array.length; i++){
+//         string += helpFunction(array[i], i);
+//     }
+//     return string;
+// }
+// console.log(effectWave('elmar', 5)); // Elmar eLmar elMar elmAr elmaR 
+
+
+// Решение 6
+const effectWave = (str) => {
+   const array = [];
+   for(let i = 0; i < str.length; i++){
+    let result = str.split('');
+    result[i] = result[i].toUpperCase();
+    array.push(result.join(''));
+   }
+   return array.join(' ')
+}
+console.log(effectWave('elmar'));  // Elmar eLmar elMar elmAr elmaR
